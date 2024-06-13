b96488ef0d357fcb547d74b26943dcd47c31f3fd
Xy258 <pc0050272005@gmail.com>
2024-06-14 01:11:22 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function showDetails(itemId) {
-    document.getElementById(itemId).style.display = 'block';
-}
-
-function closeDetails(itemId) {
-    document.getElementById(itemId).style.display = 'none';
-}
